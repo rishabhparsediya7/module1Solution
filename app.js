670e362f8a0cb9439f4d5953dbb7e755a85a91a4
Rishabh Parsediya <parsediyarishabh@gmail.com>
2021-02-18 18:24:02 +0530

    data validation

--- app.js
+++ app.js
@@ -9,17 +9,18 @@ angular.module('firstAssignment',[])
     $scope.food="";
 
     var foodItems= document.getElementById('getFoodValues').value;
-
     foodItems=foodItems.split(',');
-
-    if(foodItems.length<=3)
+    if(foodItems.length - 1 == 0){
+      $scope.food="Please enter data first";
+    }
+    else if (foodItems.length>0 && foodItems.length <=3) {
       $scope.food="Enjoy!";
+    }
     else  {
         $scope.food="Too much!";
     }
-    console.log($scope.food);
+    console.log(foodItems.length);
   };
-
 });
 
 })();
